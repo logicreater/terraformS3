0a28d72d31f6c23ccd0f86a5185bd06c2f605f9e
admin <admin@gmail.com>
2020-05-19 00:55:51 +0530

    s3bucketcreationusingterraform

--- s3.tf
+++ s3.tf
@@ -3,6 +3,10 @@
       access_key="AKIAQORZLAFTV3Y4P4EP"
       secret_key="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"
       region="ap-south-1"
+      skip_credentials_validation = true
+      skip_requesting_account_id = true
+      skip_metadata_api_check = true
+
 }
 
 resource "aws_s3_bucket" "example" {
